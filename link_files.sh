f5a5c0657fd18962397bbd00d5e294a072613583
Zach Williams <hey@zachwill.com>
2011-05-16 18:38:12 -0500

    Updated working link_files.sh script

--- link_files.sh
+++ link_files.sh
@@ -1,17 +1,26 @@
-#!bin/bash
-for file in $(ls -a | grep ^\\.[a-z])
-do
-  if [ -f $HOME/$file ]
-    then
-      echo $file already exists in the home directory.
-      # prompt for deletion
-      # then link files
-    else
-      ln -s $PWD/$file $HOME/$file
-  fi
-done
+#!/bin/bash
 
 # Easily link all the dotfiles after a `git pull`.
 #
 # Usage:
 #   bash link_files.sh
+
+link_file() { ln -s $PWD/$file $HOME/$file ; return ; }
+
+rm_and_link() { rm $HOME/$file && link_file ; return ; }
+
+prompt_deletion() {
+  read -n1 -p "Delete $HOME/$file and link new one? (y/n) " ;
+  printf "\n" ;  # Blank newline
+  [[ $REPLY == [yY] ]] && rm_and_link && echo Done || echo File not linked ;
+}
+
+for file in $(ls -a | grep ^\\.[a-z] | grep -v .git$)
+do
+  if [ -f $HOME/$file ]
+    then
+      prompt_deletion
+    else
+      link_file
+  fi
+done
