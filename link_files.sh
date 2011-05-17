ef23feb2cec2dde4b577f41e2547f25ef5905738
Zach Williams <hey@zachwill.com>
2011-05-16 22:46:00 -0500

    Added color output

--- link_files.sh
+++ link_files.sh
@@ -5,14 +5,24 @@
 # Usage:
 #   bash link_files.sh
 
+# Colors...
+GREEN='\033[32m'
+RED='\033[31m'
+BLUE='\033[34m'
+NC='\033[0m' # No Color
+
+# Output...
+DONE="${GREEN}Done${NC}"
+NOT_LINK="${RED}File not linked${NC}"
+
 link_file() { ln -s $PWD/$file $HOME/$file ; return ; }
 
 rm_and_link() { rm $HOME/$file && link_file ; return ; }
 
 prompt_deletion() {
-  read -n1 -p "Delete $HOME/$file and link new one? (y/n) " ;
+  read -n1 -p "Delete ~/$file and link new one? (y/n) " ;
   printf "\n" ;  # Blank newline
-  [[ $REPLY == [yY] ]] && rm_and_link && echo Done || echo File not linked ;
+  [[ $REPLY == [yY] ]] && rm_and_link && echo -e $DONE || echo -e $NOT_LINK ;
 }
 
 for file in $(ls -a | grep ^\\.[a-z] | grep -v .git$)
@@ -24,3 +34,4 @@ do
       link_file
   fi
 done
+echo -e "\n${BLUE}Returning to shell...${NC}\n"
